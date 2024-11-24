91872df5bc2c79fe2e20094991cddc108d8fc9fb
elaineyeh <elaine81206@gmail.com>
2024-11-24 09:34:30 +0800

    Feat(Confgi): Update next.config.mjs for static file path and update package.json for deploy folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: 'export', // 啟用靜態導出
+  basePath: '/portfolio', // 子路徑
+  assetPrefix: '/portfolio/', // 靜態資源前綴
 };
 
 export default nextConfig;
