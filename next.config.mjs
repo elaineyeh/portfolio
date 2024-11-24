fbd42d107a30cdf79968849f809926717384502c
elaineyeh <elaine81206@gmail.com>
2024-11-24 10:03:27 +0800

    Feat(Config): Update deploy comand and next.js image setting

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export', // 啟用靜態導出
   basePath: '/portfolio', // 子路徑
+  images: {
+    unoptimized: true, // 為了靜態輸出
+  },
   assetPrefix: '/portfolio/', // 靜態資源前綴
 };
 
